02fd0cfcf4efd61acd25a301f792cfc922ea9103
pshadikbasha <pshadikbasha97@gmail.com>
2022-01-26 11:54:00 +0530

    data types in python

--- durga_python/index.py
+++ durga_python/index.py
@@ -1,8 +1,3 @@
-a=10
-b=20
-c=30 if a>b else 40
-print(c)
-# the sum of two number
-print(f"the sum of two number {a+b}")
 import keyword
-print(keyword.kwlist,len(keyword.kwlist))
+print(keyword.kwlist)
+print(len(keyword.kwlist))
