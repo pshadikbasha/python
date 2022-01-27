473a2467d3bec4a86757cb0ac5820ede96a683c2
pshadikbasha <pshadikbasha97@gmail.com>
2022-01-27 21:03:09 +0530

    base conversions

--- durga_python/index.py
+++ durga_python/index.py
@@ -1,3 +1,9 @@
-import keyword
+""" import keyword
 print(keyword.kwlist)
-print(len(keyword.kwlist))
+print(len(keyword.kwlist)) """
+a=12233333333
+print(type(a))
+a=123
+print(bin(123))
+print(oct(123))
+print(hex(123))
